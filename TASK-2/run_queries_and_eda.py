60aa153139358f96aaf1141e6355d5172621f633
Harrshetha Pradeep <pradeepharrshetha@gmail.com>
2025-12-15 00:20:19 +0530

    Update run_queries_and_eda.py

--- TASK-2/run_queries_and_eda.py
+++ TASK-2/run_queries_and_eda.py
@@ -4,46 +4,37 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 import os
 
-
 sns.set_theme(style="whitegrid")
 
-# Load full data
 df = pd.read_sql_query(
     "SELECT * FROM sales_transactions WHERE order_status='Completed'",
     sqlite3.connect("sales.db"),
     parse_dates=["order_date"]
 )
 
-# ---- DASHBOARD FIGURE ----
 fig = plt.figure(figsize=(14, 9))
 
-# 1. Revenue Distribution
 plt.subplot(2, 3, 1)
 sns.histplot(df["total_amount"], bins=10)
 plt.title("Distribution of Order Value")
 
-# 2. Quantity Distribution
 plt.subplot(2, 3, 2)
 sns.histplot(df["quantity"], bins=10)
 plt.title("Distribution of Quantity Ordered")
 
-# 3. Revenue by Category
 plt.subplot(2, 3, 3)
 df.groupby("category")["total_amount"].sum().sort_values().plot(kind="barh")
 plt.title("Revenue by Category")
 
-# 4. Monthly Revenue Trend
 plt.subplot(2, 3, 4)
 monthly = df.groupby(df["order_date"].dt.to_period("M"))["total_amount"].sum()
 monthly.plot()
 plt.title("Monthly Revenue Trend")
 
-# 5. Quantity vs Revenue
 plt.subplot(2, 3, 5)
 sns.scatterplot(x="quantity", y="total_amount", data=df)
 plt.title("Quantity vs Total Amount")
 
-# 6. Correlation Heatmap
 plt.subplot(2, 3, 6)
 sns.heatmap(df[["quantity", "unit_price", "total_amount"]].corr(),
             annot=True, cmap="coolwarm")
@@ -53,3 +44,4 @@ plt.suptitle("Exploratory Data Analysis Dashboard", fontsize=12)
 plt.tight_layout()
 plt.savefig("outputs/EDA_Dashboard.png")
 plt.show()
+
